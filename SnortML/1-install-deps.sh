8e94563ceb3786db0b921b15061f6846bea6aff4
Sabeel Ansari <sabeel.ansari@intel.com>
2025-04-24 06:35:51 -0700

    Add SnortML readme
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

---
+++ SnortML/1-install-deps.sh
@@ -0,0 +1,27 @@
+# Install required dependencies for Snort3 on RL9.x
+
+# Install EPEL and enable PowerTools repositories
+dnf install epel-release
+dnf config-manager --enable crb
+
+
+# Install required build tools and dependencies
+dnf -y install cmake gcc-c++ bison flex libtool nghttp2 libnghttp2-devel \
+    libpcap-devel pcre-devel openssl-devel libdnet-devel \
+    libtirpc-devel git gcc-c++ libunwind-devel cmake hwloc-devel \
+    luajit-devel xz-devel libnfnetlink-devel libmnl-devel \
+    libnetfilter_queue-devel uuid-devel 
+
+# Following deps needed for tcmalloc
+dnf install google-perftools google-perftools-devel
+
+## libsafec-devel is not available as dnf package 
+## and is a dependency for software like ipmctl. 
+## Build from source
+
+git clone https://github.com/rurban/safeclib.git
+cd safeclib
+./build-aux/autogen.sh
+./configure
+make
+sudo make install
