ccc9ce16a1c3fd2a3f1ea52b068a4c650a211492
Sabeel Ansari <sabeel.ansari@intel.com>
2025-05-08 10:30:41 -0700

    Update flags for dnf install
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

--- SnortML/1-install-deps.sh
+++ SnortML/1-install-deps.sh
@@ -5,14 +5,14 @@ dnf install epel-release
 dnf config-manager --enable crb
 
 # Install required build tools and dependencies
-dnf -y install cmake gcc-c++ bison flex libtool nghttp2 libnghttp2-devel \
+dnf install -y cmake gcc-c++ bison flex libtool nghttp2 libnghttp2-devel \
     libpcap-devel pcre-devel openssl-devel libdnet-devel \
     libtirpc-devel git gcc-c++ libunwind-devel cmake hwloc-devel \
     luajit-devel xz-devel libnfnetlink-devel libmnl-devel \
     libnetfilter_queue-devel uuid-devel 
 
 # Following deps needed for tcmalloc
-dnf install google-perftools google-perftools-devel
+dnf install -y google-perftools google-perftools-devel
 
 ## libsafec-devel is not available as dnf package 
 ## and is a dependency for software like ipmctl. 
