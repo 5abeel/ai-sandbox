6d00535ece9b07a90cd7a965afb254226db54b8c
Sabeel Ansari <sabeel.ansari@intel.com>
2025-05-15 11:01:03 -0700

    Update rag-snort cheatsheet with perp instructions
    Signed-off-by: Sabeel Ansari <sabeel.ansari@intel.com>

--- RAG-chatbot/RAG-snort.sh
+++ RAG-chatbot/RAG-snort.sh
@@ -4,3 +4,7 @@ pip install llama-cpp-python  # For TinyLlama integration
 pip install pysyslogclient
 
 ## ACC running out of memory with above trying to build llama-cpp-python wheel files 
+
+Instructions for future:
+https://www.perplexity.ai/search/lets-start-a-new-thread-instea-TGvbWt2JS9mFBATi9ENlpA
+
